b7c9b0c211925ffcdc72d743bf32606a01f7f6ef
Alexander <asaulovalex@gmail.com>
2024-05-01 15:33:50 +0300

    added iceServers_1

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
+const ICE_S_1 = [
+  { urls: "stun:stun.l.google.com:19302" },
+  { urls: "stun:stun1.l.google.com:19302" },
+  { urls: "stun:stun2.l.google.com:19302" },
+  { urls: "stun:stun3.l.google.com:19302" },
+  { urls: "stun:stun4.l.google.com:19302" },
+  { url: "turn:turn.bistri.com:80", credential: "homeo", username: "homeo" },
+  {
+    url: "turn:turn.anyfirewall.com:443?transport=tcp",
+    credential: "webrtc",
+    username: "webrtc",
+  },
+];
+
+const ICE_S_2 = [
+  {
+    url: "turn:numb.viagenie.ca",
+    credential: "muazkh",
+    username: "webrtc@live.com",
+  },
+  {
+    url: "turn:192.158.29.39:3478?transport=udp",
+    credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+    username: "28224511:1379330808",
+  },
+  {
+    url: "turn:192.158.29.39:3478?transport=tcp",
+    credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+    username: "28224511:1379330808",
+  },
+  {
+    url: "turn:turn.bistri.com:80",
+    credential: "homeo",
+    username: "homeo",
+  },
+  {
+    url: "turn:turn.anyfirewall.com:443?transport=tcp",
+    credential: "webrtc",
+    username: "webrtc",
+  },
+];
+
 const p = new SimplePeer({
   initiator: location.hash === "#1",
   trickle: false,
 
   objectMode: true,
   config: {
-    iceServers: [
-      {
-        url: "turn:numb.viagenie.ca",
-        credential: "muazkh",
-        username: "webrtc@live.com",
-      },
-      {
-        url: "turn:192.158.29.39:3478?transport=udp",
-        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-        username: "28224511:1379330808",
-      },
-      {
-        url: "turn:192.158.29.39:3478?transport=tcp",
-        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-        username: "28224511:1379330808",
-      },
-      {
-        url: "turn:turn.bistri.com:80",
-        credential: "homeo",
-        username: "homeo",
-      },
-      {
-        url: "turn:turn.anyfirewall.com:443?transport=tcp",
-        credential: "webrtc",
-        username: "webrtc",
-      },
-    ],
+    iceServers: ICE_S_1,
   },
 });
 
